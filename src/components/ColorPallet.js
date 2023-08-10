781e8b3d318c7801eadb2856945f9d619d92df37
Neeraj Rajput <92433406+neerajkumar790652@users.noreply.github.com>
2023-08-10 20:03:29 +0530

    Update ColorPallet.js

--- src/components/ColorPallet.js
+++ src/components/ColorPallet.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 const ColorPallet = ({color_name,color_code}) => {
   return (
-    <div className='main-container'style={{width:"200px",height:"250px"}}>
-        <div className='upper'style={{width:"210px",height:"210px",backgroundColor:`${color_name}`}}></div>
+    <div className='main-container'style={{width:"210px",height:"250px"}}>
+        <div className='upper'style={{width:"210px",height:"200px",backgroundColor:`${color_name}`}}></div>
         <div className='lower' style={{width:"210px",}}>
             <h5 className='text'>{color_code}</h5>
             <p style={{color:`${color_code}`}} className="para">{color_name}</p>
